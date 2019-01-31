733e1de3c87bf8387449770b9a82377ba44396b0
School_PC <visualstudio@230-PC03>
2019-01-31 11:44:08 -0500

    With JavaScript code about all the date stuff

--- Jan31/js/main.js
+++ Jan31/js/main.js
@@ -1,1 +1,18 @@
 ﻿// Your code here!
+alert('I love ducks');
+
+var date = new Date();
+
+alert(date);
+
+alert(2 + 2);
+
+console.log("the date is" + date);
+
+document.body.innerHTML = "<h1> The date is" + date + "<h1>";
+
+document.body.innerHTML = "<h1> The date is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "<h1> ";
+
+function myfunctin() {
+    alert("This will load from the function ");
+}
